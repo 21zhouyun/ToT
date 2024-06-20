5650f82236a3177bba021e72796d42aa5e8b6389
zhouyun <21zhouyun@gmail.com>
2024-06-20 19:56:13 +0800

    working beam search version

--- solver.py
+++ solver.py
@@ -1,7 +1,10 @@
 import heapq
-    
+import logging
+import pandas as pd
 
-def beam_search(input, task, beam_size, max_depth):
+def beam_search(input, task, beam_size, max_depth, logger):
+    logger.info(f"input: {input}")
+    
     current = [(10, "", input)]
 
     solutions = []
@@ -10,33 +13,56 @@ def beam_search(input, task, beam_size, max_depth):
     for i in range(max_depth):
         next = []
 
-        print(f"Round {i} all candidates: {current}")
+        logger.info(f"Round {i} all candidates: {current}")
 
         for (score, proposal, current_state) in current:
 
-            print(f"Round {i} proposal: {proposal}, current_state: {current_state}, score: {score}")
+            logger.info(f"Round {i} proposal: {proposal}, current_state: {current_state}, score: {score}")
 
             if current_state == "24":
-                solutions.append((proposal, current_state))
+                result = task.generate_cot_proposal(proposal, current_state)
+                solutions.append(result)
                 continue
 
-            if abs(score) < 1:
+            if score < 1:
                 continue
             
             proposals, next_states = task.generate_proposals(current_state)
             for p, l in zip(proposals, next_states):
                 score = task.evaluate(l)
-                next.append((score, proposal + ';' + p, l))
+                next.append((score, proposal + '\n' + p, l))
 
-                print(f"Round {i} candidate current: {current_state}, proposal: {proposal};{p}, next_state: {l}, score: {score}")
+                logger.info(f"Round {i} candidate current: {current_state}, proposal: {";".join(proposal.split("\n"))};{p}, next_state: {l}, score: {score}")
 
         current = sorted(next, reverse=True)[:beam_size]
 
     return solutions
 
 if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.DEBUG,  # Set the log level (DEBUG, INFO, WARNING, ERROR, CRITICAL)
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',  # Define the format of log messages
+        filename='game_24.log',  # Log messages will be written to this file
+        filemode='a'  # 'a' for append mode, 'w' for write mode (overwrites the file)
+    )
+    logger = logging.getLogger('my_logger')
+    
     from task_24 import Game24
     task = Game24()
 
-    solutions = beam_search("3 3 6 10", task, beam_size=5, max_depth=4)
-    print(solutions)
+    data = pd.read_csv("data/24.csv")
+    num_puzzle = 0
+    num_correct= 0
+    for puzzle in data["Puzzles"]:
+        num_puzzle += 1
+        solutions = beam_search(puzzle, task, beam_size=5, max_depth=4, logger=logger)
+
+        if num_puzzle > 100:
+            break
+
+        for solution in solutions:
+            if task.test(solution, puzzle) > 0:
+                num_correct += 1
+                break
+
+        print(f"{num_correct} / {num_puzzle} = {num_correct/num_puzzle}")
--- task_24.py
+++ task_24.py
@@ -1,30 +1,20 @@
-import pandas as pd
-import heapq
 import game24_prompts
 import re
 import model
+import sympy
 
 class Game24():
     def __init__(self):
         self.client = model.GPT()
 
-    def extract_proposal(self, content):
-        proposal = ""
-        left = ""
-
-        splitted = content.split("(")
-        if len(splitted) < 2:
-            return proposal, left
-        
-        proposal = splitted[0]
-
+    def extract_next_state(self, content):
         # Extract what's left
         pattern = r'\(left:\s*(.*?)\)'
         match = re.search(pattern, content)
         if match:
-            left = match.group(1)
+            return match.group(1)
         
-        return proposal, left
+        return None
 
     def generate_proposals(self, puzzle):
         prompt = game24_prompts.propose_prompt.format(input=puzzle)
@@ -34,12 +24,17 @@ class Game24():
         proposals = []
         next_states = []
         for item in content.split("\n"):
-            proposal, left = self.extract_proposal(item)
-            if len(proposal) > 0 and len(left) > 0:
-                proposals.append(proposal)
-                next_states.append(left)
+            next_state = self.extract_next_state(item)
+            if next_state is not None:
+                next_states.append(next_state)
+                proposals.append(item)
 
         return proposals, next_states
+
+    def generate_cot_proposal(self, steps, puzzle):
+        prompt = game24_prompts.cot_prompt.format(input=puzzle) + "\nSteps:" + steps
+        # print(prompt)
+        return self.client.call(prompt, n=1)[0]
 
     def evaluate(self, proposal):
         prompt = game24_prompts.value_prompt.format(input=proposal)
@@ -48,13 +43,31 @@ class Game24():
         score_map = {'impossible': 0.001, 'likely': 1, 'sure': 20} # From official implementation
         contents = self.client.call(prompt, n=3)
         for content in contents:
-            name = content.split("\n")[-1]
+            # print(content)
+            name = content.split("\n")[-1].strip()
             if name in score_map:
                 score += score_map[name]
 
         return score
+    
+    def test(self, result, problem):
+        expression = result.lower().replace('answer: ', '').split("=")[0]
+
+        # Make sure solution contains the right numbers
+        numbers = re.findall(r'\d+', expression)
+        problem_numbers = re.findall(r'\d+', problem)
+
+        if sorted(numbers) != sorted(problem_numbers):
+            return 0
+
+        # Make sure steps are correct
+        if (int(sympy.simplify(expression) == 24)):
+            return 1
+        
+        return 0
 
 # task = Game24()
 # print(task.generate_proposals("3 3 6 10"))
 # print(task.extract_proposal("3 + 3 = 6 (left: 6 6 10)"))
 # print(task.evaluate("6 6 10"))
+# print(task.test("Answer: (3 + 3) * (10 - 6) = 24", "3 3 6 10"))
